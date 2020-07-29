d5e0db686aaea60888023d5c96549eb174f81e37
Gurkarn Goindi <gurkarn.goindi@berkeley.edu>
2020-07-29 18:28:26 +0530

    email test added

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ def handle_oauth_redirect():
     state = request.args.get("state")
 
     if not state_matches(state):
+        send_email("bad state")
         return json.dumps({"error": "Incorrect state parameter: " + state}), 403
 
     # Send the authorization code to Stripe's API.
@@ -70,8 +71,10 @@ def handle_oauth_redirect():
     try:
         response = stripe.OAuth.token(grant_type="authorization_code", code=code,)
     except stripe.oauth_error.OAuthError as e:
+        send_email("bad auth code")
         return json.dumps({"error": "Invalid authorization code: " + code}), 400
     except Exception as e:
+        send_email("other error")
         return json.dumps({"error": "An unknown error occurred."}), 500
 
     connected_account_id = response["stripe_user_id"]
