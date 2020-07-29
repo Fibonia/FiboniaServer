e766bf293798e8d03a6010a9e15e34390732df5b
Gurkarn Goindi <gurkarn.goindi@berkeley.edu>
2020-07-29 19:03:01 +0530

    email test added

--- app.py
+++ app.py
@@ -61,11 +61,11 @@ def create_payment():
 @app.route("/connect/oauth", methods=["GET"])
 def handle_oauth_redirect():
     # Assert the state matches the state you provided in the OAuth link (optional).
-    state = request.args.get("state")
+    #state = request.args.get("state")
 
-    if not state_matches(state):
-        send_email("bad state")
-        return json.dumps({"error": "Incorrect state parameter: " + state}), 403
+    #if not state_matches(state):
+     #   send_email("bad state")
+      #  return json.dumps({"error": "Incorrect state parameter: " + state}), 403
 
     # Send the authorization code to Stripe's API.
     code = request.args.get("code")
